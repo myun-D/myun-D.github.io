72ece170c798fc650e742236816021c403d699e5
myun-d <skye3133@gmail.com>
2023-04-21 19:50:16 +0900

    fix: build error fail fix

--- next.config.js
+++ next.config.js
@@ -17,6 +17,9 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = withMDX(nextConfig)
