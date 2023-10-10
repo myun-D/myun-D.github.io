2279d499a46475fe2b53ef2943de936157161d06
myun-D <skye3133@gmail.com>
2023-10-10 21:40:24 +0900

    fix: build error fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app"
 import { MDXProvider } from "@mdx-js/react"
-import "@/styles/fonts.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
